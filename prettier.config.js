b82ccc12b6ce144ffc353c28142bfd70a2ef8386
astrolavrov <sergey.lavrov@pimpay.ru>
2025-07-01 22:35:20 +0300

    refactor: add some fixes due to PR comments

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,9 @@
 export default {
   tabWidth: 2, // = indent_size
   useTabs: false, // = indent_style = space
-  semi: true, // ставим ; в конце строк
-  singleQuote: true, // ' вместо "
-  trailingComma: 'es5', // запятые в конце объектов/массивов (по стилю ES5)
+  semi: true, // set ; in thhe end of the lines
+  singleQuote: true, // ' instead of "
+  trailingComma: 'es5', // commas in the end of objects/arrays (ES5 style)
   bracketSpacing: true, // { foo: bar } vs {foo: bar}
   arrowParens: 'avoid', // (x) => x vs x => x
   endOfLine: 'lf', // = end_of_line
